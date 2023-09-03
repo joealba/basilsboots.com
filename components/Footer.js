12fe16fdd7fece1e6948853b7b49ed263d94d0e3
Joe Alba <joe@joealba.com>
2023-09-03 09:26:40 -0400

    working on content

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/img/logo.png" alt="Basil's Boots Logo" className={styles.logo} />
       </footer>
     </>
   )
