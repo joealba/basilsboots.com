eb417a2937d8b0430b6567b267987d2e673364fd
Joe Alba <joe@joealba.com>
2023-09-03 10:49:31 -0400

    a second fb embed

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
         </div>
         <div>
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fbasilsboots%2Fposts%2F263405643310407&show_text=true&width=500" width="500" height="452" style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fbasilsboots%2Fposts%2F263358819981756&show_text=true&width=500" width="500" height="452" style={{border: 'none', overflow: 'hidden'}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
       </main>
 
