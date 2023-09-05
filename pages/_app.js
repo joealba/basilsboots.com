78f49efb51df94b059e414f49e5a8a4b616a2204
Joe Alba <joe@joealba.com>
2023-09-05 14:30:14 -0400

    Add skeleton css framework -- trying to fix mobile styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import '@styles/normalize.css'
+import '@styles/skeleton.css'
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,27 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to Basil's Boots" />
-        <h2 style={{'text-align': 'center'}}>Hand-restored and restyled boots and accessories</h2>
-        <div style={{'text-align': 'center'}}>
-          <iframe src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fbasilsboots&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435" scrolling="yes" style={{border: 'none', overflow: 'hidden', width: '500px', height: '430px', background: 'white', float: 'left'}} ></iframe>
+        <div className="row">
+          <div className="twelve columns">
+            <h2 style={{'text-align': 'center'}}>Hand-restored and restyled boots and accessories</h2>
+          </div>
         </div>
-        <div className="description">
-          <h3>See and purchase my work on:</h3>
-          <ul>
-            <li><a href="https://www.facebook.com/basilsboots">Facebook</a></li>
-            <li><a href="https://www.instagram.com/basilsboots">Instagram</a></li>
-            <li><a href="https://www.depop.com/basilsboots/">Depop</a></li>
-          </ul>
+        <div className="row">
+          <div className="twelve columns">
+            <div style={{'text-align': 'center'}}>
+              <iframe src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fbasilsboots&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435" scrolling="yes" style={{border: 'none', overflow: 'hidden', width: '500px', height: '430px', background: 'white', float: 'left'}} ></iframe>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="twelve columns description">
+            <h3>See and purchase my work on:</h3>
+            <ul>
+              <li><a href="https://www.facebook.com/basilsboots">Facebook</a></li>
+              <li><a href="https://www.instagram.com/basilsboots">Instagram</a></li>
+              <li><a href="https://www.depop.com/basilsboots/">Depop</a></li>
+            </ul>
+          </div>
         </div>
       </main>
 
